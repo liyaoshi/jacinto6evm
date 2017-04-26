86ef7cc803ef9b425a4ee9a2c6d81edb7b1b9cbd
Vishal Mahaveer <vishalm@ti.com>
2017-04-26 16:12:56 -0400

    jacinto6evm: update cpuset configuration
    In Nougat top-app/cpus needs to be updated for cpuset
    
    Change-Id: Ibf55973a1c8087a8ebef1ce8ac9b77cf0810fb42
    Signed-off-by: Vishal Mahaveer <vishalm@ti.com>

--- init.jacinto6evmboard.cpuset.sh
+++ init.jacinto6evmboard.cpuset.sh
@@ -3,3 +3,4 @@
 # Assign all CPUs for foreground (J6 and J6 Eco have different number of cores)
 cat /sys/devices/system/cpu/present > /dev/cpuset/foreground/cpus
 cat /sys/devices/system/cpu/present > /dev/cpuset/foreground/boost/cpus
+cat /sys/devices/system/cpu/present > /dev/cpuset/top-app/cpus
