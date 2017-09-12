5a6f59fe08a8f5c93fe9c3280d82a0f46e6e3bc9
Subash Lakkimsetti <x0091084@ti.com>
2017-09-12 17:27:43 -0500

    jacinto6evm: modules: add vision and jamr3
    Add vision and jamr3 related kernel modules to the list
    of vendor modules.
    
    Change-Id: Id6bfb1ef1dafca7d87abe16730c8967895a4a7f4
    Signed-off-by: Subash Lakkimsetti <x0091084@ti.com>

--- BoardConfig.mk
+++ BoardConfig.mk
@@ -113,4 +113,10 @@ BOARD_VENDOR_KERNEL_MODULES := \
 	$(KERNELDIR)/drivers/input/touchscreen/goodix.ko \
 	$(KERNELDIR)/drivers/input/touchscreen/atmel_mxt_ts.ko \
 	$(KERNELDIR)/drivers/input/touchscreen/ldc3001_ts.ko \
-	$(KERNELDIR)/drivers/input/touchscreen/pixcir_i2c_ts.ko
+	$(KERNELDIR)/drivers/input/touchscreen/pixcir_i2c_ts.ko \
+	$(KERNELDIR)/sound/soc/codecs/snd-soc-tlv320aic3x.ko \
+	$(KERNELDIR)/drivers/gpio/gpio-mcp23s08.ko \
+	$(KERNELDIR)/drivers/gpio/gpio-pca953x.ko \
+	$(KERNELDIR)/drivers/media/i2c/ov1063x.ko \
+	$(KERNELDIR)/drivers/media/i2c/ov490.ko \
+	$(KERNELDIR)/drivers/media/i2c/tvp5158.ko
