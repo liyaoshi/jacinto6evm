liyaoshi/jacinto6evm

commit 736085b853393a0b6df68c2ac5d4416d4d1384b1

jacinto6evm: initial O changes
Initial changes for building O

Change-Id: I340f2fb47028c6eeef3be546df5659f9971f0044
Signed-off-by: Vishal Mahaveer <vishalm@ti.com>

device.mk
@@ -23,9 +23,25 @@ else
 LOCAL_KERNEL := $(TARGET_PREBUILT_KERNEL)
 endif
 
+PRODUCT_PACKAGES += \
+   android.hardware.wifi@1.0-service \
+   android.hardware.graphics.allocator@2.0-impl \
+   android.hardware.graphics.allocator@2.0-service \
+   android.hardware.graphics.mapper@2.0-impl \
+   android.hardware.memtrack@1.0-impl \
+   android.hardware.usb@1.0-service \
+   android.hardware.power@1.0-impl \
+   android.hardware.audio@2.0-impl \
+   android.hardware.audio.effect@2.0-impl \
+   android.hardware.broadcastradio@1.0-impl \
+   android.hardware.soundtrigger@2.0-impl \
+   android.hardware.keymaster@3.0-impl \
+   android.hardware.keymaster@3.0-service
+
 PRODUCT_COPY_FILES := \
 	$(LOCAL_KERNEL):kernel \
 	device/ti/jacinto6evm/tablet_core_hardware_jacinto6evm.xml:system/etc/permissions/tablet_core_hardware_jacinto6evm.xml \
+	device/ti/jacinto6evm/manifest.xml:system/vendor/manifest.xml \
 	device/ti/jacinto6evm/init.jacinto6evmboard.rc:root/init.jacinto6evmboard.rc \
 	device/ti/jacinto6evm/init.jacinto6evmboard.usb.rc:root/init.jacinto6evmboard.usb.rc \
 	device/ti/jacinto6evm/ueventd.jacinto6evmboard.rc:root/ueventd.jacinto6evmboard.rc \
@@ -83,10 +99,11 @@ PRODUCT_PACKAGES += \
 	com.android.future.usb.accessory
 
 PRODUCT_PROPERTY_OVERRIDES += \
-	ro.sf.lcd_density=160
+	ro.sf.lcd_density=240
 
 # WI-Fi
 PRODUCT_PACKAGES += \
+	wificond \
 	wpa_supplicant \
 	wpa_supplicant.conf \
 	wpa_supplicant_overlay.conf \
@@ -102,7 +119,6 @@ PRODUCT_PACKAGES += \
 	wlconf
 
 PRODUCT_PACKAGES += \
-	LegacyCamera \
 	camera_test \
 	ion_tiler_test \
 	iontest \
@@ -144,7 +160,7 @@ PRODUCT_PACKAGES += Launcher3 \
 PRODUCT_PROPERTY_OVERRIDES += \
 	media.aac_51_output_enabled=true
 
-$(call inherit-product, frameworks/native/build/tablet-7in-hdpi-1024-dalvik-heap.mk)
+#$(call inherit-product, frameworks/native/build/tablet-7in-hdpi-1024-dalvik-heap.mk)
 $(call inherit-product-if-exists, hardware/ti/dra7xx/jacinto6.mk)
 #$(call inherit-product-if-exists, hardware/ti/wpan/ti-wpan-products.mk)
 $(call inherit-product-if-exists, device/ti/proprietary-open/jacinto6/ti-jacinto6-vendor.mk)

BoardConfig.mk
@@ -16,7 +16,7 @@
 
 # These two variables are set first, so they can be overridden
 # by BoardConfigVendor.mk
-BOARD_USES_GENERIC_AUDIO := true
+#BOARD_USES_GENERIC_AUDIO := true
 #USE_CAMERA_STUB := true
 OMAP_ENHANCEMENT := true
 
@@ -33,9 +33,9 @@ TARGET_CPU_VARIANT := cortex-a15
 
 ENABLE_CPUSETS := true
 
-BOARD_HAVE_BLUETOOTH := true
-BOARD_HAVE_BLUETOOTH_TI := true
-BOARD_BLUETOOTH_BDROID_BUILDCFG_INCLUDE_DIR := device/ti/jacinto6evm/bluetooth
+#BOARD_HAVE_BLUETOOTH := true
+#BOARD_HAVE_BLUETOOTH_TI := true
+#BOARD_BLUETOOTH_BDROID_BUILDCFG_INCLUDE_DIR := device/ti/jacinto6evm/bluetooth
 TARGET_NO_BOOTLOADER := true
 
 BOARD_KERNEL_BASE := 0x80000000
@@ -75,7 +75,8 @@ BOARD_SOFTAP_DEVICE         := wl12xx_mac80211
 #COMMON_GLOBAL_CFLAGS += -DANDROID_LIB_STUB
 endif
 
-BOARD_SEPOLICY_DIRS := device/ti/jacinto6evm/sepolicy
+BOARD_SEPOLICY_DIRS += \
+	device/ti/jacinto6evm/sepolicy \
 
 # lidbrm driver
 BOARD_GPU_DRIVERS := omapdrm

full_jacinto6evm.mk
@@ -22,11 +22,11 @@
 #
 
 # Inherit from those products. Most specific first.
-$(call inherit-product, device/ti/jacinto6evm/device.mk)
 $(call inherit-product, $(SRC_TARGET_DIR)/product/full_base.mk)
+$(call inherit-product, device/ti/jacinto6evm/device.mk)
 
 PRODUCT_NAME := full_jacinto6evm
 PRODUCT_DEVICE := jacinto6evm
-PRODUCT_BRAND := Android
+PRODUCT_BRAND := TI
 PRODUCT_MODEL := jacinto6evm
 PRODUCT_MANUFACTURER := Texas_Instruments_Inc

init.jacinto6evmboard.rc
@@ -56,7 +56,7 @@ service uim /system/bin/uim-sysfs
     user root
     group bluetooth net_bt_admin
 
-service p2p_supplicant /system/bin/wpa_supplicant \
+service p2p_supplicant /vendor/bin/hw/wpa_supplicant \
     -iwlan0 -Dnl80211 -c/data/misc/wifi/wpa_supplicant.conf \
     -I/system/etc/wifi/wpa_supplicant_overlay.conf \
     -O/data/misc/wifi/sockets -pp2p_device=1\
@@ -66,7 +66,7 @@ service p2p_supplicant /system/bin/wpa_supplicant \
     disabled
     oneshot
 
-service wpa_supplicant /system/bin/wpa_supplicant \
+service wpa_supplicant /vendor/bin/hw/wpa_supplicant \
     -iwlan0 -Dnl80211 -c/data/misc/wifi/wpa_supplicant.conf \
     -I/system/etc/wifi/wpa_supplicant_overlay.conf \
     -O/data/misc/wifi/sockets \

manifest.xml
@@ -1,0 +1,20 @@
+<manifest version="1.0" type="device">
+    <hal format="hidl">
+        <name>android.hardware.graphics.allocator</name>
+        <transport>hwbinder</transport>
+        <version>2.0</version>
+        <interface>
+            <name>IAllocator</name>
+            <instance>default</instance>
+        </interface>
+    </hal>
+    <hal format="hidl">
+        <name>android.hardware.graphics.mapper</name>
+        <transport arch="32+64">passthrough</transport>
+        <version>2.0</version>
+        <interface>
+            <name>IMapper</name>
+            <instance>default</instance>
+        </interface>
+    </hal>
+</manifest>

commit 54e98268251de38663da10cd27c32612e12b5b55

jacinto6evm: include dalvik definitions
Fixes: 736085b ("jacinto6evm: initial O changes")

In initial Oreo bring up commit, this line was commented
by mistake.

Change-Id: If10291688471c3687fbe24c3d284205b03623997
Signed-off-by: Vishal Mahaveer <vishalm@ti.com>

device.mk
@@ -192,7 +192,7 @@ PRODUCT_PROPERTY_OVERRIDES += \
 	com.android.car.radio.demo=true \
 	com.android.car.radio.demo.dual=true
 
-#$(call inherit-product, frameworks/native/build/tablet-7in-hdpi-1024-dalvik-heap.mk)
+$(call inherit-product, frameworks/native/build/tablet-7in-hdpi-1024-dalvik-heap.mk)
 $(call inherit-product-if-exists, hardware/ti/dra7xx/jacinto6.mk)
 #$(call inherit-product-if-exists, hardware/ti/wpan/ti-wpan-products.mk)
 $(call inherit-product-if-exists, device/ti/proprietary-open/jacinto6/ti-jacinto6-vendor.mk)