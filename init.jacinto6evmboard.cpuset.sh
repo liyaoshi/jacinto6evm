53c8a2a0498cb7b4bce7599ec09880518721eb4b
Vishal Mahaveer <vishalm@ti.com>
2018-02-26 11:12:29 -0600

    jacinto6evm: sepolicy: changes for treble adaptations
    - Mark newly defined file_type as vendor_file_type
    - Use vendor_toolbox for cpuset script
    - Remove old crda rules that are system based.
    
    Change-Id: Iafbeeb44b7a79e358a86ec9eb3e35c4ced96cb77
    Signed-off-by: Vishal Mahaveer <vishalm@ti.com>

--- init.jacinto6evmboard.cpuset.sh
+++ init.jacinto6evmboard.cpuset.sh
@@ -1,4 +1,4 @@
-#!/system/bin/sh
+#!/vendor/bin/sh
 
 # Assign all CPUs for foreground (J6 and J6 Eco have different number of cores)
 cat /sys/devices/system/cpu/present > /dev/cpuset/foreground/cpus
